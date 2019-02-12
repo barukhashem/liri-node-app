74d65e8c1e0c9f62b16ac82dee0fd656af65972f
Baruch Hashem <barukhashem@gmail.com>
2019-02-11 22:00:15 -0700

    adding final spotify code

--- liri.js
+++ liri.js
@@ -27,72 +27,118 @@ switch (action) {
     case "spotify-this-song":
         spotifyThis(value);
         break;
+    // default:
 
     case "movie-this":
         movie(value);
         break;
+    // default:
 
     case "do-what-it-says":
-        command();
+        command(value);
         break;
 }
 
-// If the "concert" function is called...
+// If the "concert" function is called:
 function concert(artist) {
 
     // Then run a request with axios to the bandsintown.com API with the artist specified:
     axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
         function (response) {
             response.data.forEach(function (event) {
-                console.log("======================================================================================================");
+                console.log("==========================================================================");
                 console.log("Venue: " + event.venue.name);
                 console.log("City: " + event.venue.city);
                 console.log("Date: " + moment(event.datetime).format("MM/DD/YYYY"));
-                console.log("======================================================================================================");
+                console.log("==========================================================================");
             }
             )
         }
     )
 };
 
-// If the "spotify" function is called...
+// If the "spotify" function is called:
 function spotifyThis(song) {
-    spotify.search({ type: "track", query: song, limit: 1 })
-        .then(function (response) {
-            // console.log(response);
-            // console.log(JSON.stringify(response, null, 2));
-            console.log("======================================================================================================");
-            console.log("======================================================================================================");
-            console.log("Artist: " + response.tracks.items[0].artists[0].name)
-            console.log("Song: " + "'" + response.tracks.items[0].name + "'")
-            console.log("Preview URL: " + response.tracks.items[0].preview_url)
-            console.log("Album: " + response.tracks.items[0].album.name)
-            console.log("======================================================================================================");
-            console.log("======================================================================================================");
 
+    if (song.trim().length === 0) {
+        spotify.request("https://api.spotify.com/v1/tracks/3DYVWvPh3kGwPasp7yjahc")
+            .then(function (response) {
 
-            fs.writeFile("spotify-data.json", JSON.stringify(response, null, 2), function (err) {
-
-                // console.log(err);
-                // console.log("===================================");
+                console.log("==========================================================================");
+                console.log("==========================================================================");
+                console.log("Artist: " + response.album.artists[0].name);
+                console.log("Song: " + "'" + response.name + "'");
+                console.log("Preview URL: " + response.preview_url);
+                console.log("Album: " + response.album.name);
+                console.log("==========================================================================");
+                console.log("==========================================================================");
             })
-            // .catch(function (err) {
-            //     console.log(err);
-            // })
-        })
+            .catch(function (err) {
+                console.error("Error occurred: " + err);
+            });
+
+    } else {
+
+        spotify.search({ type: "track", query: song, limit: 1 })
+            .then(function (response) {
+                // console.log(response);
+                // console.log(JSON.stringify(response, null, 2));
+                console.log("==========================================================================");
+                console.log("==========================================================================");
+                console.log("Artist: " + response.tracks.items[0].artists[0].name)
+                console.log("Song: " + "'" + response.tracks.items[0].name + "'")
+                console.log("Preview URL: " + response.tracks.items[0].preview_url)
+                console.log("Album: " + response.tracks.items[0].album.name)
+                console.log("==========================================================================");
+                console.log("==========================================================================");
+
+
+                // fs.writeFile("spotify-data.json", JSON.stringify(response, null, 2), function (err) {
+
+                //     // console.log(err);
+                //     // console.log("===================================");
+                // })
+                // .catch(function (err) {
+                //     console.log(err);
+                // })
+            })
+    }
 };
 
-// If the "movie" function is called...
+// If the "movie" function is called:
 function movie(movieName) {
-    if (movieName === null) {
-        queryURL = "http://www.omdbapi.com/?t=Mr.+Nobody&y=&plot=short&apikey=trilogy"
-        return console.log(response.data);
+
+if (movieName.trim().length === 0) {
+    var queryURL = "http://www.omdbapi.com/?t=Mr.+Nobody&y=&plot=short&apikey=trilogy"
+    // .then(function (response) {
+
+        // .then(function (queryURL) {
+
+            console.log("==========================================================================");
+            console.log("Title: " + response.data.Title);
+            console.log("Year: " + response.data.Year);
+            console.log("IMDB Rating: " + response.data.imdbRating);
+            console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+            console.log("Country: " + response.data.Country);
+            console.log("Language: " + response.data.Language);
+            console.log("Plot: " + response.data.Plot);
+            console.log("Actors: " + response.data.Actors);
+            console.log("==========================================================================");
+        // })
+        // .catch(function (err) {
+        //     console.error("Error occurred: " + err);
+        // });
+
+
+    // if (movieName === " ") {
+    //     queryURL = "http://www.omdbapi.com/?t=Mr.+Nobody&y=&plot=short&apikey=trilogy"
+    //     console.log(queryURL);
     }
     else {
 
         axios.get("https://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy").then(
             function (response) {
-                console.log("======================================================================================================");
+                console.log("==========================================================================");
                 console.log("Title: " + response.data.Title);
                 console.log("Year: " + response.data.Year);
                 console.log("IMDB Rating: " + response.data.imdbRating);
@@ -101,7 +147,7 @@ function movie(movieName) {
                 console.log("Language: " + response.data.Language);
                 console.log("Plot: " + response.data.Plot);
                 console.log("Actors: " + response.data.Actors);
-                console.log("======================================================================================================");
+                console.log("==========================================================================");
             }
         )
     }
